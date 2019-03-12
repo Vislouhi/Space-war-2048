4dedfe139b971823e1cc88c1812f1a70e99c8009
Vislouhi <35730180+Vislouhi@users.noreply.github.com>
2019-03-12 18:31:57 +0500

    Update data.java

--- data.java
+++ data.java
@@ -24,3 +24,45 @@ public class DateConverter {
 	}
 
 }
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiscreteNumber {
+	private List<Integer> digits;
+	private int size;
+	
+	public DiscreteNumber() {
+		digits = new ArrayList<>();
+	}
+	
+	public void makeDigit(int d) {
+		int number = d;
+		while(number>0) {
+			digits.add(number%10);
+			number/=10;
+		}
+		size=digits.size();
+		
+	}
+	
+	public int get(int i) {
+		return digits.get(i);
+		
+		
+		
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void print() {
+		for (Integer i:digits) {
+			System.out.println("digit "+i);
+		}
+	}
+
+}
